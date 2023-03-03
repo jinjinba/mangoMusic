7ece697f3080c54cdccb33d214ca405bf622434b
서정민 <ywcsky2@gmail.com>
2023-03-03 16:18:04 +0900

    Initial commit

---
+++ src/main/java/com/kh/mango/mangoMusic/MangoMusicApplication.java
@@ -0,0 +1,13 @@
+package com.kh.mango.mangoMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/mango/mangoMusic/MangoMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.mango.mangoMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
