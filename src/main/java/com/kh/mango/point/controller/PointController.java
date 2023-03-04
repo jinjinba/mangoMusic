c9868efc6203ac3a7c8b4e8fe40ff3689f314a50
서정민 <ywcsky2@gmail.com>
2023-03-04 23:18:08 +0900

    23-03-04 init : 정적 리소스 수정 및 클래스 수정시 자동 반영되도록 디펜던시 추가 및 설정

--- src/main/java/com/kh/mango/point/controller/PointController.java
+++ src/main/java/com/kh/mango/point/controller/PointController.java
@@ -1,4 +1,13 @@
 package com.kh.mango.point.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PointController {
+
+    @GetMapping("/point")
+    public String pointUpdate(){
+        return "mypage";
+    }
 }
