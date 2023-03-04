f376c3e5c8761e19cb3e5de866ad4f0a27851a3a
서정민 <ywcsky2@gmail.com>
2023-03-04 21:45:14 +0900

    23-03-04 init : test

--- src/main/java/com/kh/mango/user/controller/UserController.java
+++ src/main/java/com/kh/mango/user/controller/UserController.java
@@ -1,4 +1,11 @@
 package com.kh.mango.user.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class UserController {
+
+    @GetMapping("asd")
+    public void test(){
+
+    }
 }
