a07b1c4d4333873f9d794bab38d2af0e0c8a88e9
jinjinba <rlwlsdlfkso1@gmail.com>
2023-03-06 14:11:47 +0900

    Changes

--- src/main/java/com/kh/mango/user/controller/UserController.java
+++ src/main/java/com/kh/mango/user/controller/UserController.java
@@ -2,12 +2,17 @@ package com.kh.mango.user.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
-    @GetMapping("asd")
-    public void test(){
-
+    @PostMapping("/login")
+    public String test(){
+    return "login";
+    }
+    @PostMapping ("/register")
+    public String test1(){
+        return "register";
     }
 }
