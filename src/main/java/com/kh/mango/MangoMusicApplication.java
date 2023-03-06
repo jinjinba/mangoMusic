978d40d8ff2d7a1a2c188112a19b32d022aa14d0
서정민 <ywcsky2@gmail.com>
2023-03-06 12:51:16 +0900

    2023-03-06 init : db설정 업데이트 / 마이바티스 설정 추가

--- src/main/java/com/kh/mango/MangoMusicApplication.java
+++ src/main/java/com/kh/mango/MangoMusicApplication.java
@@ -1,8 +1,8 @@
-package com.kh.mango.user;
+package com.kh.mango;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class MangoMusicApplication {
 
