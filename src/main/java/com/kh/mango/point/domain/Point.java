f6c755701691ff2386aef224b261a01cc25b0ec1
jinjinba <rlwlsdlfkso1@gmail.com>
2023-03-06 10:29:08 +0900

    Changes

--- src/main/java/com/kh/mango/point/domain/Point.java
+++ src/main/java/com/kh/mango/point/domain/Point.java
@@ -1,4 +1,15 @@
 package com.kh.mango.point.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
 public class Point {
+    private int userNo;
+    private int pointVal;
+
 }
