d9cfa8b243fa343456d3f2354891f757695af5c3
서정민 <ywcsky2@gmail.com>
2023-03-13 16:36:38 +0900

    2023-03-10 myPage 충전 및 출금 개선

--- src/main/java/com/kh/mango/point/controller/PointController.java
+++ src/main/java/com/kh/mango/point/controller/PointController.java
@@ -6,10 +6,7 @@ import com.kh.mango.user.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,37 +27,28 @@ public class PointController {
         return "refundPoint";
     }
 
-    @PostMapping("/chargePointAction")
-    public String pointChargeAction(@RequestParam("pointVal") int pointVal, Model model, @SessionAttribute("loginUser") User user){
-            Map<String, Integer> map = new HashMap<String,Integer>();
-            Point point = new Point(user.getUserNo(), pointVal);
-            int result = pService.updateAddPoint(point);
-            Point pointValue = pService.selectPoint(user.getUserNo());
-            if(result > 0){
-                map.put("pointVal",pointValue.getPointVal());
-                return null;
-            }else {
-                model.addAttribute("fal",1);
-                return null;
-            }
+    @PostMapping("/ajaxAddPoint")
+    @ResponseBody
+    public String ajaxAddPoint(int userNo, int pointVal){
+        Point point = new Point(userNo, pointVal);
+        int result = pService.updateAddPoint(point);
+        if(result > 0){
+            Point point1 = pService.selectPoint(userNo);
+            return String.valueOf(point1.getPointVal());
+        }
+        return null;
     }
 
-    @PostMapping("/ajaxPoint")
-    public void ajaxPoint(@RequestParam("userNo")int userNo, @RequestParam("pointVal") int pointVal){
-        System.out.println(userNo);
-        System.out.println(pointVal);
-    }
-
-    @GetMapping("/refundPointAction")
-    public String pointRefund(@RequestParam("pointVal") int pointVal, Model model, @SessionAttribute("loginUser") User user){
-        Point point = new Point(user.getUserNo(), pointVal);
+    @PostMapping("/ajaxRefundPoint")
+    @ResponseBody
+    public String ajaxRefundPoint(int userNo, int pointVal){
+        Point point = new Point(userNo, pointVal);
         int result = pService.updateRefundPoint(point);
         if(result > 0){
-            model.addAttribute("com","window.close()");
-            return "refundPoint";
-        }else {
-            model.addAttribute("fal",1);
-            return "refundPoint";
+            Point point1 = pService.selectPoint(userNo);
+            return String.valueOf(point1.getPointVal());
         }
+        return null;
     }
+
 }
--- src/main/resources/static/mypage.js
+++ src/main/resources/static/mypage.js
@@ -3,3 +3,121 @@ $(".que").click(function() {
     $(this).toggleClass('on').siblings().removeClass('on');
     $(this).next(".anw").siblings(".anw").slideUp(300); // 1개씩 펼치기
 });
+//  포인트 잔량 정규 표현식
+var pointVal = document.querySelector(".point-val").value;
+document.querySelector(".point-val-re").innerHTML = pointVal.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+
+// 포인트 충전 내역 정규표현식
+var pointVal2 = document.querySelectorAll(".pointRecord-val");
+for (var a = 0; a < pointVal2.length; a++) {
+    document.querySelectorAll(".pointRecord-val-re")[a].innerHTML = pointVal2[a].value.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") + " Mango";
+}
+
+function modal(id) {
+    var zIndex = 9999;
+    var modal = document.getElementById(id);
+
+    // 모달 div 뒤에 희끄무레한 레이어
+    var bg = document.createElement('div');
+    bg.setStyle({
+        position: 'fixed',
+        zIndex: zIndex,
+        left: '0px',
+        top: '0px',
+        width: '100%',
+        height: '100%',
+        overflow: 'auto',
+        // 레이어 색갈은 여기서 바꾸면 됨
+        backgroundColor: 'rgba(0,0,0,0.4)'
+    });
+    document.body.append(bg);
+
+    // 닫기 버튼 처리, 시꺼먼 레이어와 모달 div 지우기
+    modal.querySelector('#modal_close_btn').addEventListener('click', function () {
+        bg.remove();
+        modal.style.display = 'none';
+    });
+    // modal.querySelector('#modal_close_btn-2').addEventListener('click', function () {
+    //     bg.remove();
+    //     modal.style.display = 'none';
+    // });
+
+    modal.setStyle({
+        position: 'fixed',
+        display: 'block',
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+
+        // 시꺼먼 레이어 보다 한칸 위에 보이기
+        zIndex: zIndex + 1,
+
+        // div center 정렬
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        msTransform: 'translate(-50%, -50%)',
+        webkitTransform: 'translate(-50%, -50%)'
+    });
+}
+
+// Element 에 style 한번에 오브젝트로 설정하는 함수 추가
+Element.prototype.setStyle = function (styles) {
+    for (var k in styles) this.style[k] = styles[k];
+    return this;
+};
+
+document.getElementById('popup_open_btn').addEventListener('click', function () {
+    // 모달창 띄우기
+    modal('my_modal');
+});
+document.getElementById('popup_open_btn-2').addEventListener('click', function () {
+    // 모달창 띄우기
+    modal('my_modal-2');
+});
+
+function pointAddFunc() {
+    const pointData = {
+        "userNo": parseInt($('.userNo').val()),
+        "pointVal": parseInt($('#point-add-input').val())
+    };
+    $.ajax(
+        {
+            type: "POST",
+            url: "/ajaxAddPoint",
+            data: pointData,
+            success: function (data) {
+                $('.point-val-re').text(data.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ","));
+            }
+        }
+    )
+    $('#modal_close_btn').trigger("click");
+}
+
+function pointRefundFunc() {
+    const pointData = {
+        "userNo": parseInt($('.userNo').val()),
+        "pointVal": parseInt($('#point-refund-input').val())
+    };
+    $.ajax(
+        {
+            type: "POST",
+            url: "/ajaxRefundPoint",
+            data: pointData,
+            success: function (data) {
+                $('.point-val-re').text(data.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ","));
+            }
+        }
+    )
+    $('#modal_close_btn').trigger("click");
+}
+
+// 음악 정보 api 로 가져오기
+
+test = $('#likeTag').val();
+var request = new XMLHttpRequest();
+request.open('GET', 'https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=45ac2dc0a0b1bad3966314cc210f8c49&artist=cher&track=believe&format=json');
+request.send();
+request.onload = function () {
+    var songInfo = JSON.parse(request.response).track;
+    document.querySelector("#songName").innerHTML = songInfo.name;
+    document.querySelector("#songArtist").innerHTML = songInfo.artist.name;
+}
