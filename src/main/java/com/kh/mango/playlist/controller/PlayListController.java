4c91d61fa0e25128a788ff7eb05ce7b6ab48146c
s1212w <s1212w@naver.com>
2023-03-19 21:56:05 +0900

    3.19

--- src/main/java/com/kh/mango/playlist/controller/PlayListController.java
+++ src/main/java/com/kh/mango/playlist/controller/PlayListController.java
@@ -1,4 +1,17 @@
 package com.kh.mango.playlist.controller;
 
+import com.kh.mango.playlist.service.PlayListService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
 public class PlayListController {
+
+    @GetMapping("/followPlaylist")
+    public String followPlaylist() {
+        return "followPlaylist";
+    }
 }
